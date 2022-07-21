2907d42325a9bc3d1de7c8d1cd485f8df3438b0f
don_durito <calen36@gmail.com>
2022-07-21 22:43:40 +0300

    add tasks

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def input_task_name(message: types.Message, state: FSMContext,  *args, **k
     else:
         await state.update_data(dirname=dirname)
         await TaskCreation.input_cad_nums.set()
-        text = f"Имя каталога: {dirname}\nВведите кадастровые номера"
+        text = f"Введите кадастровые номера"
     await telegram_bot.send_message(message.from_user.id, text, reply_markup=kbd, parse_mode="HTML")
 
 
@@ -81,11 +81,12 @@ async def input_cad_nums(message: types.Message, state: FSMContext,  *args, **kw
     found_cad_nums = extract_cad_nums(message.text)
     try:
         if found_cad_nums:
-            found, not_found, text = {}, [], ''
+            found, not_found, text, n_copied, years_count = {}, [], '', 0, {}
             for cad_num in found_cad_nums:
                 results = docfetcher_search(f'"{cad_num}"')
                 if results:
                     found[cad_num] = [r for r in results if r.getType() == 'xml']
+                    n_copied += len(found[cad_num])
                 else:
                     not_found.append(cad_num)
             if not_found:
@@ -93,32 +94,38 @@ async def input_cad_nums(message: types.Message, state: FSMContext,  *args, **kw
                 for cad_num in not_found:
                     text += f'{cad_num}\n'
                 text += '</code>\n'
+            else:
+                text = 'Все номера найдены в базе'
             if found:
                 state_data = await state.get_data()
                 task_path = os.path.join(TASKS_ROOT, state_data['dirname'])
                 if not os.path.exists(task_path):
                     os.makedirs(task_path)
-                text += f'Создан каталог:\n<code>{task_path}</code>\n'
-                text += 'Найдены в базе, скопированы:\n<code>'
+
                 for cad_num in found:
-                    text += f'{cad_num}\n'
                     for file in found[cad_num]:
                         ext_date = get_date(file.getPathStr())
+                        year = ext_date[:4]
+                        if year in years_count:
+                            years_count[year] += 1
+                        else:
+                            years_count[year] = 1
                         ext_dir_path = os.path.join(task_path, ext_date)
                         if not os.path.exists(ext_dir_path):
                             os.makedirs(ext_dir_path)
                         shutil.copy(file.getPathStr(), ext_dir_path)
-                        text += f'\t\t{ext_date} {file.getFilename()}\n'
+
+                text += f'Создан каталог:\n<code>{task_path}</code>\n'
+                text += f'Скопирован{"ы" if n_copied > 1 else ""} {n_copied} файл{"а" if n_copied%10 in (2,3,4) else ""}{"ов" if n_copied in (5,6,7,8,9,0) else ""}\n'
+                text += f'По годам:<code>\n'
+                for year in sorted(years_count):
+                    text += f'{year}: {years_count[year]}'
                 text += '</code>\n'
         else:
             text = "Кадастровых номеров не найдено. Задание отменено."
     except Py4JNetworkError:
         text = 'DocFetcher не запущен'
-    while text:
-        newtext = text[4095:]
-        text = text[:4095]
-        await telegram_bot.send_message(message.from_user.id, text, reply_markup=kbd,)
-        text = newtext
+    await telegram_bot.send_message(message.from_user.id, text, parse_mode="HTML", reply_markup=kbd,)
     await state.finish()
 
 
